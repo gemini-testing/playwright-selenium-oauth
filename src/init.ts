d40bfa52ffa379860641caed24e2cecd4d09011d
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2023-06-15 16:26:08 +0600

    fix

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,13 @@
+import { homedir } from "os";
 import { readToken } from "./read-token";
+import { join } from "path";
 
 export interface IInitOptions {
     tokenFilePath?: string;
 }
 
 export async function init(options?: IInitOptions) {
-    const path = options?.tokenFilePath ?? "~/.playwright/oauth_token";
+    const path = options?.tokenFilePath ?? join(homedir(), ".playwright/oauth_token");
     const token = await readToken(path);
     process.env.SELENIUM_REMOTE_HEADERS = `{'Authorization':'OAuth ${token}'}`;
 }
