da88b8bcdc76338e0ae47f788e5e7385251f1edc
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2023-06-22 16:38:25 +0600

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,4 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2022,
     },
-    overrides: [
-        {
-            files: ["*.js"],
-            rules: {
-                "@typescript-eslint/no-var-requires": "off",
-            },
-        },
-        {
-            files: ["test/**"],
-            rules: {
-                "@typescript-eslint/no-empty-function": "off",
-                // For convenient casting of test objects
-                "@typescript-eslint/no-explicit-any": "off",
-            },
-        },
-    ],
 };
